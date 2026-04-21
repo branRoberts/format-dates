6445bd63e0b74cb57e0d6ed9d9cb938328c31bc1
branRoberts <brandonkrob12@gmail.com>
2026-04-21 14:37:30 -0400

    Second Commit
    Added lines to make the current date formatted in a specific way

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
+    public static void main(String[] args) {
+        String dateFormat = "dd/MM/yyyy";
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
+
+        System.out.println(today.format(formatter));
+        sout
+    }
 }
